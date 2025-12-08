d0a49d5b8a344609f7e0bb9f989f81ec19da4444
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-12-08 07:09:56 +0600

    update useAxiosSecure for handle server error

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,28 +1,55 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import useAuth from './useAuth'
+import { useNavigate } from 'react-router-dom';
 
 const axiosSecure = axios.create({
     baseURL: `http://localhost:3000`
 })
 
 function useAxiosSecure() {
-    const { user } = useAuth();
+    const { user, logOut } = useAuth();
+    const navigate = useNavigate();
 
     useEffect(() => {
         const requestInterceptor = axiosSecure.interceptors.request.use(
             (config) => {
-                config.headers.Authorization = `Bearer ${user?.accessToken}`;
+                if (user?.accessToken) {
+                    config.headers.Authorization = `Bearer ${user?.accessToken}`;
+                }
                 return config;
             },
             (error) => Promise.reject(error)
         );
 
+        const responseInterceptor = axiosSecure.interceptors.response.use(
+            (response) => response,
+
+            (error) => {
+                const status = error?.response?.status;
+
+                // 403: Forbidden
+                if (status === 403) {
+                    navigate('/*')
+                }
+
+                // 401: Unauthorized - token invalid/expired
+                else if (status === 401) {
+                    logOut()
+                        .then(() => navigate("/login"))
+                        .catch(() => { });
+                }
+
+                return Promise.reject(error);
+            }
+        )
+
         return () => {
             axiosSecure.interceptors.request.eject(requestInterceptor);
+            axiosSecure.interceptors.response.eject(responseInterceptor);
         };
 
-    }, [user?.accessToken]);
+    }, [user?.accessToken, logOut, navigate]);
 
     return axiosSecure;
 }
