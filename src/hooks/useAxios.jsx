abdc6f8b1fa432a84c108f57219671fcb0aa9607
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-12-17 19:03:30 +0600

    add server deploying url

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React from 'react'
 
 const axiosInstance = axios.create({
-    baseURL: `http://localhost:3000`
+    baseURL: `https://zap-shift-server-cfiv.onrender.com`
 })
 function useAxios() {
   return axiosInstance
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth'
 import { useNavigate } from 'react-router-dom';
 
 const axiosSecure = axios.create({
-    baseURL: `http://localhost:3000`
+    baseURL: `https://zap-shift-server-cfiv.onrender.com`
 })
 
 function useAxiosSecure() {
